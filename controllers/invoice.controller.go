cd46d21e8aa3b93cc90517ce87240c74afec4217
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:07:36 +0630

    feat(invoice): basic controller added

--- controllers/invoice.controller.go
+++ controllers/invoice.controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetInvoices() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetInvoices"})
+	}
+}
+func GetInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "get invoice"})
+	}
+}
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "CreateInvoice"})
+	}
+}
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "UpdateInvoice"})
+	}
+}
