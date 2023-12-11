f9d543d0aa82c2ea0fe8c13a918605604b65973d
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:07:41 +0630

    feat(menu): basic controller added

--- controllers/menu.controller.go
+++ controllers/menu.controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetMenus"})
+	}
+}
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetMenu"})
+	}
+}
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "CreateMenu"})
+	}
+}
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "UpdateMenu"})
+	}
+}
