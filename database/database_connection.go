9f707339aa46d004f61272bb4449d0d7f1d81686
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:08:24 +0630

    update(database): db setup finisehd

--- database/database_connection.go
+++ database/database_connection.go
@@ -1,1 +1,35 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017"
+	fmt.Println(MongoDB)
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
