62ec78356a09eb6ed8e70d8e0699f517e18308ce
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:08:00 +0630

    feat(order-item): basic controller added

--- controllers/order-items.controller.go
+++ controllers/order-items.controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetOrderItems"})
+	}
+}
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetOrderItem"})
+	}
+}
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "CreateOrderItem"})
+	}
+}
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "UpdateOrderItem"})
+	}
+}
