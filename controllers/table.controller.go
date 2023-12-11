7ca0cd682c723fff8c9681e463560c7cc915fb5d
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:08:07 +0630

    feat(table): basic controller added

--- controllers/table.controller.go
+++ controllers/table.controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetTables"})
+	}
+}
+func GetTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetTable"})
+	}
+}
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "CreateTable"})
+	}
+}
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "UpdateTable"})
+	}
+}
