a3af40546f0d0689d4eceecfad0b60f851720bcd
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-11 21:07:29 +0630

    feat(food): basic controller added

--- controllers/food.controller.go
+++ controllers/food.controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "GetFoods"})
+	}
+}
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "get food"})
+	}
+}
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "CreateFood"})
+	}
+}
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "UpdateFood"})
+	}
+}
