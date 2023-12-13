ae4ef8308bf437e0aca4e9e1496f670cd14fb64a
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-13 22:13:52 +0630

    feat(menu): get menu / menus controller finished

--- controllers/menu.controller.go
+++ controllers/menu.controller.go
@@ -1,15 +1,46 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kaungmyathan22/golang-restaurant-management-app/database"
+	"github.com/kaungmyathan22/golang-restaurant-management-app/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var menuCollection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "GetMenus"})
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu items"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "GetMenu"})
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		id := c.Param("menu_id")
+		var menu models.Menu
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": id}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the menu"})
+			return
+		}
+		c.JSON(http.StatusOK, menu)
 	}
 }
 func CreateMenu() gin.HandlerFunc {
